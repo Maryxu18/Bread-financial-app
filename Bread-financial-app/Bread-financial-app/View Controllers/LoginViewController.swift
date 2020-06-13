7955784b550a6157eb0eb780b24939577853a0f9
Mary Xu <maryxyx14@gmail.com>
2020-06-13 15:59:52 -0400

    Set up login, sign up, forget password, and home page storyboards

---
+++ Bread-financial-app/Bread-financial-app/View Controllers/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Bread-financial-app
+//
+//  Created by Amanda on 2020-02-24.
+//  Copyright © 2020 Amanda. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    //MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: Actions
+    @IBAction func onClickSignUp(_ sender: UIButton) {
+        print("sign up tapped")
+    }
+    
+    @IBAction func onClickLogin(_ sender: UIButton) {
+        print("login tapped")
+    }
+}
+
+
+
---
+++ Bread-financial-app/Bread-financial-app/View Controllers/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  Bread
+//
+//  Created by Mary Xu on 2020-06-13.
+//  Copyright © 2020 Amanda. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
